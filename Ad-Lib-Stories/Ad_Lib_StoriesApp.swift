9ae0c0eec429512780ba6fd4313835843fcf9195
Josh Dunlap <joshua.c.dunlap@wmich.edu>
2024-01-21 17:41:36 -0500

    Initial Commit

---
+++ Ad-Lib-Stories/Ad_Lib_StoriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ad_Lib_StoriesApp.swift
+//  Ad-Lib-Stories
+//
+//  Created by Josh Dunlap on 1/21/24.
+//
+
+import SwiftUI
+
+@main
+struct Ad_Lib_StoriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Ad-Lib-StoriesUITests/Ad_Lib_StoriesUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Ad_Lib_StoriesUITestsLaunchTests.swift
+//  Ad-Lib-StoriesUITests
+//
+//  Created by Josh Dunlap on 1/21/24.
+//
+
+import XCTest
+
+final class Ad_Lib_StoriesUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
